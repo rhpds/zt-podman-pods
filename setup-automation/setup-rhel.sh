c61738803fbbd858b57b57ecd9cfb370b2086e29
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:21 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,3 +1,7 @@
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 !#/bin/bash
 LOG=/root/post-run.log
 
